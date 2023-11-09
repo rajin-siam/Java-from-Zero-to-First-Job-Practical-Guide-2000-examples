8ca706bdef6221c15695c4758113bcb32b19167e
Md. Rajin Mashrur Siam <67337679+rajin-siam@users.noreply.github.com>
2023-11-09 18:48:07 +0600

    Add files via upload

---
+++ firstproject/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"firstproject","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"firstproject","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ firstproject/src/firstproject/HelloWorld.java
@@ -0,0 +1,28 @@
+package firstproject;
+	/**
+	 * 
+	 * Hello World Application
+	 * 
+	 * @author Md. Rajin Mashrur Siam
+	 *
+	 */
+
+	public class HelloWorld {
+	
+	/**
+	 * The entry point to the program.
+	 * 
+	 * @param args input arguments
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		/*
+		 * Please, do not remove
+		 * this code.
+		 */
+
+	}
+
+}
